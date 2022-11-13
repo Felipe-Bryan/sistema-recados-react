e0eb5ef2375f0c9aa42f1884f60d368db47555e3
Felipe Bryan <felipe.bryan@live.com>
2022-11-13 08:27:30 -0300

    estrutura de pastas modificada, projeto reiniciado

---
+++ src/shared/components/AppStyled.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface AppStyledProps {
+  mode: 'dark' | 'light';
+}
+
+const AppStyled = styled.div<AppStyledProps>`
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.background[props.mode]};
+  color: ${(props) => props.theme.color[props.mode]};
+`;
+
+export default AppStyled;
---
+++ src/shared/components/DisplayStyled.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DisplayStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default DisplayStyled;
